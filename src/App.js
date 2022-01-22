8b182d447561e6710c58c0a9ff3c2f8a35d3ae4c
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-01-23 04:36:29 +0530

    style: added style to App to center the card component & feat: created a text card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TextCard from './components/cards/textCard/TextCard';
 
 function App() {
   return (
     <div className="App">
-      
+      <TextCard />
     </div>
   );
 }
---
+++ src/components/cards/textCard/TextCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./TextCard.css"
+
+function TextCard() {
+  return <div className='card'>
+      
+  </div>;
+}
+
+export default TextCard;
