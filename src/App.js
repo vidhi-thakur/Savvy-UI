060825339da6d32f7530e9ac91d785b9ebc0ce48
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-01-23 17:20:18 +0530

    feat: completed basic card

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import TextCard from './components/cards/textCard/TextCard';
 function App() {
   return (
     <div className="App">
-      <TextCard />
+      <TextCard
+        heading="This is card heading"
+        subHeading="This is card subheading"
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        btn={<button>LINK</button>}
+      />
     </div>
   );
 }
