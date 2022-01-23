68c828894485aaade31d811709f708f5f6460684
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-01-23 20:10:35 +0530

    [card 2] image card with/without header
    created a card with an option to add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import TextCard from './components/cards/textCard/TextCard';
+import ImageCard from './components/cards/imageCard/ImageCard';
 
 function App() {
   return (
     <div className="App">
-      <TextCard
+      <ImageCard
         heading="This is card heading"
         subHeading="This is card subheading"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        content="Lorem Ipsum is simply dummy lorem ipsum has been the industry's standard dummy text text of the printing and typesetting industry."
         btn={<button>LINK</button>}
       />
     </div>
